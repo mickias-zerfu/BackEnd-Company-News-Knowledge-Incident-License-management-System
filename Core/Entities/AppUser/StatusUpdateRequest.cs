fd668353596bfb6ceeb109536b7756adfc43c76d
Mikiyas.Zerfu <Mikiyas.Zerfu@zemenbank.com>
2024-05-14 16:24:18 +0300

    implement subadmin functionalities

---
+++ Core/Entities/AppUser/StatusUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.AppUser
+{
+    public class StatusUpdateRequest
+    {        public string Id { get; set; }
+    }
+}
